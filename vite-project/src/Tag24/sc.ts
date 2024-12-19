a293c736d40357833191722d3c07e111585921ae
PatrickZantz <p.zantz@gmail.com>
2024-12-19 09:04:44 -0300

    ohne node_modules

---
+++ vite-project/src/Tag24/sc.ts
@@ -0,0 +1,88 @@
+console.log('Welcome :)');
+
+let dogName = 'elf'; // zu nem string kann kein wert addiert werden. Außer vllt auf nen pointer im cache.. 
+
+let seventeen = 0;  // -.-
+
+let sum = 2;
+
+console.log("addition: " + sum);
+sum += 2;
+console.log("sub: " + sum);
+sum -= 1;
+console.log("mult: " + sum);
+sum *= 4;
+console.log("divi: " + sum);
+sum /= 2;
+console.log("mod: " + sum);
+
+// Addition
+let addition_operator: number = 1 + 1;
+console.log('addition: ' + addition_operator);
+
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
+console.log('subtraktion: ' + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator: number = 2 * 2;
+console.log('multiplication: ' + multiplication_operator);
+
+// Division
+let division_operator: number = 4 / 2;
+console.log('division: ' + division_operator);
+
+// Modulo: zeigt an, was der Rest sein würde.
+let modulo_operator: number = 14 % 5;
+console.log('modulus: ' + modulo_operator);
+
+let x = 20;
+let y = 30;
+
+console.log(x +y);
+console.log(y -x);
+console.log(y *x);
+console.log(x / y);
+let z = 10;
+let resultOne = (x * y) / z;
+
+console.log(resultOne);
+
+let a = 15;
+let b = 9;
+
+console.log(a%b);
+let c = 20;
+let resultTwo = (a + b) * c;
+console.log(resultTwo);
+
+console.log(c++);
+console.log(c--);
+
+console.log(++c); // kennste den schon?
+console.log(--c);
+
+let result3 = b-a;
+console.log(result3);
+
+console.log(resultOne%resultTwo);
+
+let score: number = 5 + 5 * 10;  
+console.log('Ergebnis: ' + score);
+
+score = 0;  
+score = score + 10;  
+console.log('Ergebnis: ' + score);
+
+score += 10;  
+console.log('Ergebnis: ' + score)
+
+let zahl = 1;  
+zahl = zahl + 1;  
+zahl += 1;
+
+zahl++;  
+console.log('increment: ' + zahl);
+
+zahl--;  
+console.log('dekrement: ' + zahl);
