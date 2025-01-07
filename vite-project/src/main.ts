a5e07271f128d06fb6260dcce6662e1bbcbaf482
PatrickZantz <p.zantz@gmail.com>
2025-01-07 07:56:00 -0300

    check

---
+++ vite-project/src/main.ts
@@ -0,0 +1,8 @@
+// src/main.js
+console.log('Vite-App gestartet')
+
+// Hier können Sie Ihre Anwendungslogik implementieren
+const app = document.createElement('div')
+app.id = 'app'
+app.textContent = 'Meine Vite-App ohne index.html'
+document.body.appendChild(app)
